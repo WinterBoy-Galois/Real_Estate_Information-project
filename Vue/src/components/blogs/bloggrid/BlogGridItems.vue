94d4ab6319b9e299faa479c15d7f113d1c882869
LeeSunSo <tjstn921030@gmail.com>
2020-06-23 16:14:21 +0900

    BlogGridItems-infinite Scroll
    What to fix
    - focusing doesn't work well

--- Vue/src/components/blogs/bloggrid/BlogGridItems.vue
+++ Vue/src/components/blogs/bloggrid/BlogGridItems.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="row">
+  <div class="row" style="text-align:center">
     <div :class="'column-td-6 col-lg-'+columns" v-for="(item, index) in items" v-bind:key="index">
-      <div class="card-item">
+      <div class="card-item" id="wrapper">
         <div class="card-img-box">
           <img :src="require(`@/assets/images/${item.imgurl}`)" alt="">
         </div>
         <div style="margin-top:-40px" class="card-content">
-          <h4 class="card-meta"><v-icon class="bl-icon" name="clock"></v-icon> {{item.buildYear}}</h4>
+          <h4 class="card-meta">{{index}}<v-icon class="bl-icon" name="clock"></v-icon> {{item.buildYear}}</h4>
           <h3 class="card-title"><router-link :to="`/blog-single?no=${item.no}`">{{item.aptName}}</router-link></h3>
           <ul class="list-items">
             <li class="card-dot-active"><v-icon class="bl-icon" name="user"></v-icon> in <a :href="item.no">{{item.dong}}
@@ -14,16 +14,18 @@
             </a></li>
             <li><v-icon class="bl-icon" name="circle"></v-icon> {{item.dealAmount}} ￦ </li>
           </ul>
-          <router-link :to="`/blog-single?no=${item.no}`" class="template-btn btn-layout-small">read more</router-link>
+          <router-link :to="`/blog-single?no=${item.no}`" class="template-btn btn-layout-small" force-use-infinite-wrapper="#wrapper">read more</router-link>
         </div>
       </div>
     </div>
+    <infinite-loading @infinite="infiniteHandler" spinner="circles" infinite-scroll-disabled="busy"></infinite-loading>
 
   </div>
 </template>
 
 <script>
 import http from "@/util/http-common";
+import InfiniteLoading from 'vue-infinite-loading';
 
     export default {
       name: "BlogGridItems",
@@ -31,33 +33,51 @@ import http from "@/util/http-common";
       data() {
         return {
           items: [
-            // {
-            //   img: require("@/assets/images/img1.jpg"),
-            //   title: "How we can tak care of our health against Virus",
-            //   time: "4 min read",
-            //   data: "Jan 8, 2019",
-            //   author: "TechyDevs",
-            //   authorUrl: "http://techydevs.com",
-            // }
-          ]
+          ],
+          limit: 0,
+          scrollY:0,
+          timer: null
         }
       },
+      components:{
+        InfiniteLoading
+      },
       created() {
-        http
-        .get("/aptinfo")
-        .then(({ data }) => {
-          console.log(data);
-          for(let i = 0; i < data.length; i++){
-            if(!data[i].imgurl){
-              data[i].imgurl = "다세대주택.jpg";
+        http.get("/aptlist/"+this.limit)
+        .then((data) =>{
+          console.log(data.data);
+          for(let i = 0; i < data.data.length; i++){
+            if(!data.data[i].imgurl){
+              data.data[i].imgurl = "다세대주택.jpg";
             }
-            console.log(data[i].imgurl);
+            else if(data.data[i].imgurl.includes("jfif")){
+              console.log(data.data[i].imgurl);
+              data.data[i].imgurl = "다세대주택.jpg";
+            }
+            console.log(data.data[i].imgurl);
           }
-          this.items = data;
+          this.items = data.data;
         })
-        .catch(() => {
-          alert("에러가 발생했습니다.");
-        });
+        window.addEventListener('scroll', this.handleScroll)
+        // http
+        // .get("/aptinfo")
+        // .then(({ data }) => {
+        //   console.log(data);
+        //   for(let i = 0; i < data.length; i++){
+        //     if(!data[i].imgurl){
+        //       data[i].imgurl = "다세대주택.jpg";
+        //     }
+        //     console.log(data[i].imgurl);
+        //   }
+        //   this.items = data;
+        // })
+        // .catch(() => {
+        //   alert("에러가 발생했습니다.");
+        // });
+      },
+      beforeDestroy: function () {
+    // 핸들러 제거하기(컴포넌트 또는 SPA의 경우 절대 잊지 말아 주세요!)
+        window.removeEventListener('scroll', this.handleScroll)
       },
       methods:{
         dongList(message){
@@ -96,7 +116,46 @@ import http from "@/util/http-common";
             .catch(() => {
               alert("에러가 발생했습니다.");
             });
-        }
+        },
+        infiniteHandler($state){
+          http.get("/aptlist/" + (this.limit+10))
+          .then(data => {
+            setTimeout(() => {
+              if(data.data.length){
+                for(let i = 0; i < data.data.length; i++){
+                  if(!data.data[i].imgurl){
+                    data.data[i].imgurl = "다세대주택.jpg";
+                  }
+                  else if(data.data[i].imgurl.includes("jfif")){
+                    console.log(data.data[i].imgurl);
+                    data.data[i].imgurl = "다세대주택.jpg";
+                  }
+                }               
+                this.items = this.items.concat(data.data);
+                $state.loaded();
+                this.limit += 10;
+                if(this.items.length / 10 == 0){
+                  $state.complete();
+                }
+                this.handleScroll();
+              }else{
+                $state.complete();
+              }
+            }, 3000)
+          }).catch(error =>{
+            console.error(error);
+          })
+
+        },
+        handleScroll: function () {
+          if (this.timer === null) {
+            this.timer = setTimeout(function () {
+              this.scrollY = window.scrollY
+              clearTimeout(this.timer)
+              this.timer = null
+            }.bind(this), 200)
+          }
+        }       
       }
       
     }
